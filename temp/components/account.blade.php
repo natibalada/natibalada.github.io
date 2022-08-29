bf7d72358e3f4b9ec3c0fbd9a819768d02d2fe3a
natibalada <utsuhobestbirb@gmail.com>
2022-08-29 17:50:31 +0200

    working on showing products

--- temp/components/account.blade.php
+++
@@ -1,9 +0,0 @@
-<p class="formlogin" style="font-size:1em">Hola {{ $user->name }}!</p><br>
-@if ($user->name == 'Administrador')
-<a href="{{ url('/backend') }}" id="backendLink"><button id="backendLink">Backend</button></a>
-@endif
-<a href="{{ url('/usuario') }}" id="settingsLink"><button id="settingsBtn">Mis datos</button></a>
-<form id="formlogout">
-    @csrf
-    <input class="formlogout button" type="button" id="logout" name="logout" value="Logout" >
-</form>
